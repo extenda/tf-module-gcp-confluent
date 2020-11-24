fd1a73b3408a8592fba8c4d7b3bbea80b15b2f1f
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>
2020-11-24 13:58:32 +0200

    chore: Upgrade confluent provider

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     confluentcloud = {
       source  = "Mongey/confluentcloud"
-      version = "0.0.5"
+      version = "0.0.6"
     }
     google-beta = {
       source = "hashicorp/google-beta"
