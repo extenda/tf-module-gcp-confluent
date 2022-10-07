8e3cc861d838f399a4269670673c8446c4455fbb
Anastasiia Kovtun <Anastasiia.Kovtun@extendaretail.com>
2022-10-07 12:36:47 +0100

    chore: Update provider version (#7)

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     confluentcloud = {
       source  = "Mongey/confluentcloud"
-      version = "0.0.6"
+      version = "0.0.15"
     }
     google-beta = {
       source = "hashicorp/google-beta"
